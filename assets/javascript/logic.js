af4ec97860d9001a5a23b35187f7f109a711c6c0
Olga <oserenchenko@gmail.com>
2018-07-02 20:34:41 -0600

    set up initial files

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBHXITJYd5PDGHXRg8nWq7cqBiXiXzlZmI",
+    authDomain: "train-scheduler-20530.firebaseapp.com",
+    databaseURL: "https://train-scheduler-20530.firebaseio.com",
+    projectId: "train-scheduler-20530",
+    storageBucket: "train-scheduler-20530.appspot.com",
+    messagingSenderId: "355321402197"
+  };
+  firebase.initializeApp(config);
