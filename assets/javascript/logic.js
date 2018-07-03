7decdf7022ac5660857f473d7d655557c3e7d48d
Olga <oserenchenko@gmail.com>
2018-07-02 21:32:44 -0600

    grabbing form values and saving to firebase

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,3 +8,34 @@
     messagingSenderId: "355321402197"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  $('#newTrainSubmit').on('click', function (event) {
+    event.preventDefault();
+
+    var name = $('#nameInput').val().trim();
+    var destination = $('#destInput').val().trim();
+    var firstTime = $('#firstTimeInput').val().trim();
+    var frequency = $('#frequencyInput').val().trim();
+
+    database.ref().push({
+      name: name,
+      destination: destination,
+      firstTime: firstTime,
+      frequency: frequency
+    })
+
+    // Clears all of the text-boxes
+    $("#nameInput").val("");
+    $("#destInput").val("");
+    $("#firstTimeInput").val("");
+    $("#frequencyInput").val("");
+  });
+
+  database.ref().on('child_added', function(childSnapshot) {
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var trainFirstTime = childSnapshot.val().firstTime;
+    var trainFreq = childSnapshot.val().frequency;
+  })
